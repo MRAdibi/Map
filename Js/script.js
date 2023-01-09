7b9114bf8951548029185e4d464a830aa4689993
mohammad <m.r.adibi125@gmail.com>
2023-01-09 16:41:06 +0330

    add some info-box to the UI and get them ready for developmen

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,19 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+// ////// this section will add soon
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//         const cords = [latitude, longitude]
+//         const map = L.map('map').setView(cords, 17);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+//         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         }).addTo(map);
+
+//         L.marker(cords).addTo(map)
+//             .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//             .openPopup();
+//     }, function () { })
+// }
+
