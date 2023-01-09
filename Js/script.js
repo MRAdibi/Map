8a14f7cb48689a3852c69ee5e6b68e27c4e7d0b9
mohammad <m.r.adibi125@gmail.com>
2023-01-09 18:33:02 +0330

    add the popup to the map

--- Js/script.js
+++ Js/script.js
@@ -7,21 +7,34 @@ const locations = document.querySelector('.locations')
 
 ///////////////// main map API call /////////////////
 const startApp = function () {
+    // check is geolocation works or not 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-
+            // set latitude and longitude to use on the map
             const latitude = position.coords.latitude;
             const longitude = position.coords.longitude;
             const cords = [latitude, longitude]
-            const map = L.map('map').setView(cords, 17);
 
+            const map = L.map('map').setView(cords, 17);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            L.marker(cords).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
+            map.on('click', function (eventMap) {
+                console.log(eventMap)
+                const lat = eventMap.latlng.lat
+                const lng = eventMap.latlng.lng
+
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(L.popup({
+                        maxWidth: 250,
+                        maxHeight: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                    }).setContent("Hello World"))
+                    .openPopup();
+            })
         }, function () { })
     }
 }
