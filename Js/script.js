01d3f9af6f70e7eb15b53c0b9e394d33f751ab84
mohammad <m.r.adibi125@gmail.com>
2023-01-11 17:42:06 +0330

    created a mechanism to add a new location

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,15 @@
 const infoBoxes = document.querySelectorAll('.tutorial')
 const startBtn = document.querySelector('.start-map-section')
 const locations = document.querySelector('.locations')
-
-
+const locationAddForm = document.querySelector('.add-location')
+const addLocationForm = document.querySelector('form')
+const typeSelect = document.querySelector('.type')
+const inputDistance = document.querySelector('.distance input')
+const inputDuration = document.querySelector('.duration input')
+const inputCadence = document.querySelector('.cadence input')
+const inputElevation = document.querySelector('.elevation input')
+let map;
+let mapEvent;
 
 ///////////////// main map API call /////////////////
 const startApp = function () {
@@ -15,25 +22,16 @@ const startApp = function () {
             const longitude = position.coords.longitude;
             const cords = [latitude, longitude]
 
-            const map = L.map('map').setView(cords, 17);
+            map = L.map('map').setView(cords, 17);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            map.on('click', function (eventMap) {
-                console.log(eventMap)
-                const lat = eventMap.latlng.lat
-                const lng = eventMap.latlng.lng
+            map.on('click', function (mapE) {
+                mapEvent = mapE;
+                locationAddForm.classList.remove("add-location-hidden");
+                inputDistance.focus();
 
-                L.marker([lat, lng])
-                    .addTo(map)
-                    .bindPopup(L.popup({
-                        maxWidth: 250,
-                        maxHeight: 100,
-                        autoClose: false,
-                        closeOnClick: false,
-                    }).setContent("Hello World"))
-                    .openPopup();
             })
         },
             function () {
@@ -42,6 +40,34 @@ const startApp = function () {
     }
 }
 ///////////////// end call API ///////////////// 
+
+
+// handle add a new location
+addLocationForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+    const lat = mapEvent.latlng.lat
+    const lng = mapEvent.latlng.lng
+
+    // Display the marker
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            maxHeight: 100,
+            autoClose: false,
+            closeOnClick: false,
+        }).setContent("Hello World"))
+        .openPopup();
+})
+
+
+// handle the changing type
+typeSelect.addEventListener('change', () => {
+    inputCadence.closest('.cadence').classList.toggle("hidden")
+    inputElevation.closest('.elevation').classList.toggle("hidden")
+})
+
 
 
 startBtn.querySelector('button').addEventListener("click", () => {
